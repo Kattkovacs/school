f7e8c6a3f56f5d0e929bbccf304cac2aedef757f
Kattkovacs <kattkovacs@gmail.com>
2020-12-05 17:58:08 +0100

    add @PreAuthorize instead .antMatchers

--- src/main/java/com/katt/school/student/StudentManagementController.java
+++ src/main/java/com/katt/school/student/StudentManagementController.java
@@ -1,6 +1,6 @@
 package com.katt.school.student;
 
-import com.fasterxml.jackson.core.JsonToken;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -17,24 +17,28 @@ public class StudentManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents(){
         System.out.println("getAllStudents");
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerStudent(@RequestBody Student student){
         System.out.println("registerStudent");
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId){
         System.out.println("deleteStudent");
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student){
         System.out.println("updateStudent");
         System.out.println(String.format("%s %s", studentId, student));
